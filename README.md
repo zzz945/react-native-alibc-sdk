5e4f11da2038b5d5ca38cc91e87722a1c2b1eb73
et <et@cpe-172-89-1-27.socal.res.rr.com>
2017-04-07 11:15:44 +0800

    add taobao authentication/authorization for android

--- README.md
+++ README.md
@@ -36,10 +36,68 @@
 
 
 ## Usage
-```javascript
-import RNAlibcSdk from 'react-native-alibc-sdk';
 
-// TODO: What to do with the module?
-RNAlibcSdk;
-```
+### 环境配置
+
+进入阿里百川开发者控制台 -> 创建应用 -> 在我的产品后台开通百川电商SDK -> 在API申请开通初级电商能力。
+
+#### Android
+1. 上传用于调试的app-debug.apk（发布时再上传签名的apk）， 获取安全图片放在（res/drawable/yw_1222.jpg）。
+2. AndroidManifest.xml:
+	```
+	<manifest ...
+		xmlns:tools="http://schemas.android.com/tools"
+			...
+		<application
+			...
+		android:allowBackup="true"
+		tools:replace="android:allowBackup">
+		...
+	</manifest>
+	```
+3. build.gradle:
+	```
+	...
+	allprojects {
+		repositories {
+			...
+			maven {
+				url "http://repo.baichuan-android.taobao.com/content/groups/BaichuanRepositories/"
+			}
+			...
+		}
+	}
+	...
+	```
+### 示例代码
+
+Demo：https://github.com/zzz945/RNAlibcSdkDemo
+
+### 淘宝登录授权
+
+1. 引入sdk
+	```
+		import RNAlibcSdk from 'react-native-alibc-sdk';
+	```
+2. 初始化sdk(此方法为异步， 确保回调执行后再进行下一步操作)
+	```
+		AlibcSdk.init(
+			() => console.log("init success"),
+			(code, msg) => console.log(code + ":" + msg)
+    	);
+	```
+3. 唤起手淘app进行授权登录， 获取用户个人信息。 
+	```
+		AlibcSdk.login(
+			(userInfo) => console.log(userInfo),
+			(code, msg) => console.log(code + ":" + msg)
+		);
+	```
+3. 务必调用logout，否则app处在login状态将不会再次唤起手淘app进行授权登录。
+	```
+		AlibcSdk.logout(
+			() => console.log("logout success"),
+			(code, msg) => console.log(code + ":" + msg)
+		);
+	```
   
--- android/src/main/java/com/daiyan/RNAlibcSdkModule.java
+++ android/src/main/java/com/daiyan/RNAlibcSdkModule.java
@@ -5,8 +5,24 @@ import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
+import com.facebook.react.bridge.BaseActivityEventListener;
+import com.facebook.react.bridge.ActivityEventListener;
+import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.bridge.Arguments;
+
+import android.app.Activity;
+import android.content.Intent;
 
 import android.support.annotation.Nullable;
+
+import com.ali.auth.third.login.callback.LogoutCallback;
+import com.ali.auth.third.ui.context.CallbackContext;
+import com.alibaba.baichuan.android.trade.adapter.login.AlibcLogin;
+import com.ali.auth.third.core.model.Session;
+import com.alibaba.baichuan.android.trade.callback.AlibcLoginCallback;
+import com.alibaba.baichuan.android.trade.AlibcTradeSDK;
+import com.alibaba.baichuan.android.trade.callback.AlibcTradeInitCallback;
+
 import android.util.Log;
 
 public class RNAlibcSdkModule extends ReactContextBaseJavaModule {
@@ -14,9 +30,17 @@ public class RNAlibcSdkModule extends ReactContextBaseJavaModule {
   private final ReactApplicationContext reactContext;
   private static final String TAG = "RNAlibcSdkModule";
 
+  private final ActivityEventListener mActivityEventListener = new BaseActivityEventListener() {
+    @Override
+    public void onActivityResult(Activity activity, int requestCode, int resultCode, Intent intent) {
+      CallbackContext.onActivityResult(requestCode, resultCode, intent);
+    }
+  };
+
   public RNAlibcSdkModule(ReactApplicationContext reactContext) {
     super(reactContext);
     this.reactContext = reactContext;
+    reactContext.addActivityEventListener(mActivityEventListener);
   }
 
   @Override
@@ -24,8 +48,69 @@ public class RNAlibcSdkModule extends ReactContextBaseJavaModule {
     return "RNAlibcSdk";
   }
 
+  /**
+  * 初始化
+  */
   @ReactMethod
-  public void login(@Nullable Callback callback) {
-    callback.invoke();
+  public void init(final Callback successCallback, final Callback failCallback) {
+      AlibcTradeSDK.asyncInit(reactContext, new AlibcTradeInitCallback() {
+        @Override
+        public void onSuccess() {
+            successCallback.invoke();
+        }
+
+        @Override
+        public void onFailure(int code, String msg) {
+            failCallback.invoke(code, msg);
+        }
+    }); 
+  }
+            
+  /**
+  * 登录
+  */
+  @ReactMethod
+  public void login(final Callback successCallback, final Callback failCallback) {
+      AlibcLogin alibcLogin = AlibcLogin.getInstance();
+
+      alibcLogin.showLogin(getCurrentActivity(), new AlibcLoginCallback() {
+          @Override
+          public void onSuccess() {
+            Session session = AlibcLogin.getInstance().getSession();
+            Log.v(TAG, "获取淘宝用户信息: "+ AlibcLogin.getInstance().getSession());
+            WritableMap map = Arguments.createMap();
+            map.putString("nick", session.nick);
+            map.putString("avatarUrl", session.avatarUrl);
+            map.putString("openId", session.openId);
+            map.putString("openSid", session.openSid);
+            successCallback.invoke(map);
+          }
+          @Override
+          public void onFailure(int code, String msg) {
+            Log.v(TAG, "login fail");
+            failCallback.invoke(code, msg);
+          }
+      });
+  }
+
+
+  /**
+  * 退出登录
+  */
+  @ReactMethod
+  public void logout(final Callback successCallback, final Callback failCallback) {
+      AlibcLogin alibcLogin = AlibcLogin.getInstance();
+
+      alibcLogin.logout(getCurrentActivity(), new LogoutCallback() {
+          @Override
+          public void onSuccess() {
+            successCallback.invoke();
+          }
+
+          @Override
+          public void onFailure(int code, String msg) {
+            failCallback.invoke(msg);
+          }
+      });
   }
 }

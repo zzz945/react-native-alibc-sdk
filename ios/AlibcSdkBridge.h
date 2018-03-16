d4463c0246ab1fbe70fdf832becfaf9971139a66
wanxsb <wanxsb@users.noreply.github.com>
2018-03-17 01:04:09 +0800

    fix bug

--- ios/AlibcSdkBridge.h
+++ ios/AlibcSdkBridge.h
@@ -6,10 +6,10 @@
 //  Copyright © 2017年 Facebook. All rights reserved.
 //
 
-#if __has_include("RCTBridgeModule.h")
-#import "RCTBridgeModule.h"
-#else
+#if __has_include(<React/RCTBridgeModule.h>)
 #import <React/RCTBridgeModule.h>
+#else
+#import "RCTBridgeModule.h"
 #endif
 
 #import <Foundation/Foundation.h>
